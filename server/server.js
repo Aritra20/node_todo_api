bb1d16d2b090866eac10974e6758b2d942275989
Aritra20 <aritradmukherjee@gmail.com>
2017-04-10 21:43:06 +0530

    Add Get todos route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -21,7 +20,17 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
-module.exports = app;
+
+module.exports = {app};
