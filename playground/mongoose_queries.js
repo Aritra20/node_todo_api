25307dd5121b2e3fa08642fe64f1e7f07d30d926
Aritra20 <aritradmukherjee@gmail.com>
2017-04-11 23:35:43 +0530

    Add get todos ID

--- playground/mongoose_queries.js
+++ playground/mongoose_queries.js
@@ -29,7 +29,7 @@ const {User} = require('./../server/models/user');
 //   console.log('Todo By Id', todo);
 // }).catch((e) => console.log(e));
 
-User.findById('57bdb0fcdedf88540bfa2d66').then((user) => {
+User.findById('58ecca1e608499124c446210').then((user) => {
   if (!user) {
     return console.log('Unable to find user');
   }
